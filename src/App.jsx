447a93a5f7e1759de378ac8104a85afdd4d5f3e2
aswathm78 <aswathm78@gmail.com>
2022-07-18 16:01:56 +0530

    2 files modified, 2 added, 1 deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 import './App.css';
+import Recent from './components/recent';
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>
-  <img href="https://image.shutterstock.com/image-vector/recent-event-history-600w-1221604048.jpg" alt="recent"/>
-  </>;
+  return (
+    <>
+      <Recent />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/recent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Recent = () => {
+  <h1>Hi hello</h1>;
+};
+
+export default Recent;
